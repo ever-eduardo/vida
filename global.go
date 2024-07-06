2d6bcca4f9b92b7a4d01ad2dbb59eddd48123824
Ever Eduardo Velazco <106024807+ever-eduardo@users.noreply.github.com>
2024-07-06 08:59:36 -0500

    Add formatter and basic print

--- global.go
+++ global.go
@@ -1,12 +1,25 @@
 package vida
 
+import (
+	"fmt"
+)
+
 var NilValue = Nil{}
 
 func loadPrelude() map[string]Value {
 	p := make(map[string]Value)
-	p["print"] = NilValue
+	p["print"] = GoFn(gfnPrint)
 	p["len"] = NilValue
+	p["append"] = NilValue
+	p["load"] = NilValue
 	p["type"] = NilValue
 	p["assert"] = NilValue
 	return p
+}
+
+func gfnPrint(args ...Value) (Value, error) {
+	for i := range args {
+		fmt.Println(args[i].String())
+	}
+	return NilValue, nil
 }
