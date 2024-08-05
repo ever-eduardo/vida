749ed24cfe7f74d16dc387c2739fd7d9df313b07
Ever Eduardo Velazco <106024807+ever-eduardo@users.noreply.github.com>
2024-08-05 01:07:49 -0500

    Update vm

--- vm.go
+++ vm.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math"
 
+	"github.com/ever-eduardo/vida/token"
 	"github.com/ever-eduardo/vida/verror"
 )
 
@@ -41,11 +42,248 @@ func (vm *VM) Run() (Result, error) {
 	vm.Frame.code = vm.Module.MainFunction.CoreFn.Code
 	vm.Frame.lambda = vm.Module.MainFunction
 	vm.Frame.stack = vm.Stack[:]
-	ip := 8
+	ip := 1
+	var i, op, A, B, P uint64
 	for {
-		op := vm.Frame.code[ip]
+		i = vm.Frame.code[ip]
+		op = i >> shift56
+		A = i >> shift16 & clean16
+		B = i & clean16
+		P = i >> shift32 & clean24
 		ip++
 		switch op {
+		case storeG:
+			if P == 1 {
+				vm.Module.Store[B] = vm.Module.Konstants[A]
+			} else {
+				vm.Module.Store[B] = vm.Frame.stack[A]
+			}
+		case loadG:
+			vm.Frame.stack[B] = vm.Module.Store[A]
+		case loadF:
+			vm.Frame.stack[B] = vm.Frame.lambda.Free[A]
+		case loadK:
+			vm.Frame.stack[B] = vm.Module.Konstants[A]
+		case move:
+			vm.Frame.stack[B] = vm.Frame.stack[A]
+		case storeF:
+			vm.Frame.lambda.Free[B] = vm.Frame.stack[A]
+		case check:
+			if P == 0 {
+				if !vm.Frame.stack[A].Boolean() {
+					ip = int(B)
+				}
+			}
+		case jump:
+			ip = int(B)
+		case binopG:
+			val, err := vm.Module.Store[A].Binop(P>>shift16, vm.Module.Store[P&clean16])
+			if err != nil {
+				return Failure, verror.New(vm.Module.Name, "Runtime error", verror.RunTimeErrMsg, math.MaxUint16)
+			}
+			vm.Frame.stack[B] = val
+		case binop:
+			val, err := vm.Frame.stack[A].Binop(P>>shift16, vm.Frame.stack[P&clean16])
+			if err != nil {
+				return Failure, verror.New(vm.Module.Name, "Runtime error", verror.RunTimeErrMsg, math.MaxUint16)
+			}
+			vm.Frame.stack[B] = val
+		case binopK:
+			val, err := vm.Frame.stack[P&clean16].Binop(P>>shift16, vm.Module.Konstants[A])
+			if err != nil {
+				return Failure, verror.New(vm.Module.Name, "Runtime error", verror.RunTimeErrMsg, math.MaxUint16)
+			}
+			vm.Frame.stack[B] = val
+		case binopQ:
+			val, err := vm.Module.Konstants[A].Binop(P>>shift16, vm.Frame.stack[P&clean16])
+			if err != nil {
+				return Failure, verror.New(vm.Module.Name, "Runtime error", verror.RunTimeErrMsg, math.MaxUint16)
+			}
+			vm.Frame.stack[B] = val
+		case eq:
+			val := vm.Frame.stack[A].Equals(vm.Frame.stack[P&clean16])
+			if P>>shift16 == uint64(token.NEQ) {
+				val = !val
+			}
+			vm.Frame.stack[B] = val
+		case eqG:
+			val := vm.Module.Store[A].Equals(vm.Module.Store[P&clean16])
+			if P>>shift16 == uint64(token.NEQ) {
+				val = !val
+			}
+			vm.Frame.stack[B] = val
+		case eqK:
+			val := vm.Frame.stack[P&clean16].Equals(vm.Module.Konstants[A])
+			if P>>shift16 == uint64(token.NEQ) {
+				val = !val
+			}
+			vm.Frame.stack[B] = val
+		case eqQ:
+			val := vm.Module.Konstants[A].Equals(vm.Frame.stack[P&clean16])
+			if P>>shift16 == uint64(token.NEQ) {
+				val = !val
+			}
+			vm.Frame.stack[B] = val
+		case prefix:
+			val, err := vm.Frame.stack[A].Prefix(P)
+			if err != nil {
+				return Failure, verror.New(vm.Module.Name, "Runtime error", verror.RunTimeErrMsg, math.MaxUint16)
+			}
+			vm.Frame.stack[B] = val
+		case iGet:
+			var val Value
+			var err error
+			if P>>shift16 == 0 {
+				val, err = vm.Frame.stack[P].IGet(vm.Frame.stack[A])
+			} else {
+				val, err = vm.Frame.stack[P&clean16].IGet(vm.Module.Konstants[A])
+			}
+			if err != nil {
+				return Failure, verror.New(vm.Module.Name, "Runtime error", verror.RunTimeErrMsg, math.MaxUint16)
+			}
+			vm.Frame.stack[B] = val
+		case iSet:
+			var err error
+			if P>>shift16 == 0 {
+				err = vm.Frame.stack[P].ISet(vm.Frame.stack[A], vm.Frame.stack[B])
+			} else {
+				err = vm.Frame.stack[P&clean16].ISet(vm.Frame.stack[A], vm.Module.Konstants[B])
+			}
+			if err != nil {
+				return Failure, verror.New(vm.Module.Name, "Runtime error", verror.RunTimeErrMsg, math.MaxUint16)
+			}
+		case iSetK:
+			var err error
+			if P>>shift16 == 0 {
+				err = vm.Frame.stack[P].ISet(vm.Module.Konstants[A], vm.Frame.stack[B])
+			} else {
+				err = vm.Frame.stack[P&clean16].ISet(vm.Module.Konstants[A], vm.Module.Konstants[B])
+			}
+			if err != nil {
+				return Failure, verror.New(vm.Module.Name, "Runtime error", verror.RunTimeErrMsg, math.MaxUint16)
+			}
+		case slice:
+			val, err := vm.processSlice(P, A)
+			if err != nil {
+				return Failure, verror.New(vm.Module.Name, "Runtime error", verror.RunTimeErrMsg, math.MaxUint16)
+			}
+			vm.Frame.stack[B] = val
+		case list:
+			xs := make([]Value, P)
+			F := A
+			for i := 0; i < int(P); i++ {
+				xs[i] = vm.Frame.stack[F]
+				F++
+			}
+			vm.Frame.stack[B] = &List{Value: xs}
+		case object:
+			vm.Frame.stack[B] = &Object{Value: make(map[string]Value)}
+		case forSet:
+			if _, isInteger := vm.Frame.stack[B].(Integer); !isInteger {
+				return Failure, verror.RuntimeError
+			}
+			if _, isInteger := vm.Frame.stack[B+1].(Integer); !isInteger {
+				return Failure, verror.RuntimeError
+			}
+			if v, isInteger := vm.Frame.stack[B+2].(Integer); !isInteger {
+				return Failure, verror.RuntimeError
+			} else {
+				if v == 0 {
+					return Failure, verror.RuntimeError
+				}
+			}
+			ip = int(A)
+		case iForSet:
+			iterable := vm.Frame.stack[A]
+			if !iterable.IsIterable() {
+				return Failure, verror.RuntimeError
+			}
+			vm.Frame.stack[B] = iterable.Iterator()
+			ip = int(P)
+		case forLoop:
+			i := vm.Frame.stack[B].(Integer)
+			e := vm.Frame.stack[B+1].(Integer)
+			s := vm.Frame.stack[B+2].(Integer)
+			if s > 0 {
+				if i < e {
+					vm.Frame.stack[B+3] = i
+					i += s
+					vm.Frame.stack[B] = i
+					ip = int(A)
+				}
+			} else {
+				if i > e {
+					vm.Frame.stack[B+3] = i
+					i += s
+					vm.Frame.stack[B] = i
+					ip = int(A)
+				}
+			}
+		case iForLoop:
+			i, _ := vm.Frame.stack[B].(Iterator)
+			if i.Next() {
+				vm.Frame.stack[B+1] = i.Key()
+				vm.Frame.stack[B+2] = i.Value()
+				ip = int(A)
+				continue
+			}
+		case fun:
+			fn := &Function{CoreFn: vm.Module.Konstants[A].(*CoreFunction)}
+			if fn.CoreFn.Free > 0 {
+				var free []Value
+				for i := 0; i < fn.CoreFn.Free; i++ {
+					if fn.CoreFn.Info[i].IsLocal {
+						free = append(free, vm.Frame.stack[fn.CoreFn.Info[i].Index])
+					} else {
+						free = append(free, vm.Frame.lambda.Free[fn.CoreFn.Info[i].Index])
+					}
+				}
+				fn.Free = free
+			}
+			vm.Frame.stack[B] = fn
+		case call:
+			val := vm.Frame.stack[B]
+			if !val.IsCallable() {
+				return Failure, verror.RuntimeError
+			}
+			if fn, ok := val.(*Function); ok {
+				if int(A) != fn.CoreFn.Arity {
+					return Failure, verror.RuntimeError
+				}
+				if vm.fp >= frameSize {
+					return Failure, verror.RuntimeError
+				}
+				if fn == vm.Frame.lambda && vm.Frame.code[ip] == ret {
+					for i := 0; i < int(A); i++ {
+						vm.Frame.stack[i] = vm.Frame.stack[int(B)+1+i]
+					}
+					ip = 0
+					continue
+				}
+				vm.Frame.ip = ip
+				vm.Frame.ret = int(B)
+				bs := vm.Frame.bp
+				vm.fp++
+				vm.Frame = &vm.Frames[vm.fp]
+				vm.Frame.lambda = fn
+				vm.Frame.bp = bs + int(B) + 1
+				vm.Frame.code = fn.CoreFn.Code
+				vm.Frame.stack = vm.Stack[vm.Frame.bp:]
+				ip = 0
+			} else if fn, ok := val.(GFn); ok {
+				v, err := fn(vm.Frame.stack[B+1 : B+A+1]...)
+				if err != nil {
+					return Failure, err
+				}
+				vm.Frame.stack[B] = v
+			}
+		case ret:
+			val := vm.Frame.stack[B]
+			vm.fp--
+			vm.Frame = &vm.Frames[vm.fp]
+			ip = vm.Frame.ip
+			vm.Frame.stack = vm.Stack[vm.Frame.bp:]
+			vm.Frame.stack[vm.Frame.ret] = val
 		case end:
 			return Success, nil
 		default:
