0c89f1efc8830010a8299e79fb1384e596764c6c
Ever Eduardo Velazco <106024807+ever-eduardo@users.noreply.github.com>
2024-08-19 21:02:50 -0500

    Init impl math stdlib

---
+++ stdlib/math.go
@@ -0,0 +1,79 @@
+package stdlib
+
+import (
+	"math"
+
+	"github.com/ever-eduardo/vida"
+)
+
+func generateMath() vida.Value {
+	m := &vida.Object{Value: make(map[string]vida.Value)}
+	m.Value["pi"] = vida.Float(math.Pi)
+	m.Value["phi"] = vida.Float(math.Phi)
+	m.Value["e"] = vida.Float(math.E)
+	m.Value["inf"] = mathInf(math.Inf)
+	m.Value["isNan"] = mathIsNan(math.IsNaN)
+	m.Value["isInf"] = mathIsInf(math.IsInf)
+	m.Value["nan"] = mathNan(math.NaN)
+	m.Value["ceil"] = mathFromFloatToFloat(math.Ceil)
+	m.Value["floor"] = mathFromFloatToFloat(math.Floor)
+	m.Value["round"] = mathFromFloatToFloat(math.Round)
+	m.Value["roundToEven"] = mathFromFloatToFloat(math.RoundToEven)
+	m.Value["abs"] = mathFromFloatToFloat(math.Abs)
+	m.Value["sqrt"] = mathFromFloatToFloat(math.Sqrt)
+	m.Value["cbrt"] = mathFromFloatToFloat(math.Cbrt)
+	m.UpdateKeys()
+	return m
+}
+
+func mathInf(fn func(int) float64) vida.GFn {
+	return func(args ...vida.Value) (vida.Value, error) {
+		if len(args) > 0 {
+			if v, ok := args[0].(vida.Integer); ok {
+				return vida.Float(fn(int(v))), nil
+			}
+		}
+		return vida.NilValue, nil
+	}
+}
+
+func mathIsNan(fn func(float64) bool) vida.GFn {
+	return func(args ...vida.Value) (vida.Value, error) {
+		if len(args) > 0 {
+			if v, ok := args[0].(vida.Float); ok {
+				return vida.Bool(fn(float64(v))), nil
+			}
+		}
+		return vida.NilValue, nil
+	}
+}
+
+func mathIsInf(fn func(float64, int) bool) vida.GFn {
+	return func(args ...vida.Value) (vida.Value, error) {
+		if len(args) > 1 {
+			if v, ok := args[0].(vida.Float); ok {
+				if i, oki := args[1].(vida.Integer); oki {
+					return vida.Bool(fn(float64(v), int(i))), nil
+				}
+			}
+		}
+		return vida.NilValue, nil
+	}
+}
+
+func mathNan(fn func() float64) vida.GFn {
+	return func(args ...vida.Value) (vida.Value, error) {
+		return vida.Float(fn()), nil
+	}
+}
+
+func mathFromFloatToFloat(fn func(float64) float64) vida.GFn {
+	return func(args ...vida.Value) (vida.Value, error) {
+		if len(args) > 0 {
+			if v, ok := args[0].(vida.Float); ok {
+				return vida.Float(fn(float64(v))), nil
+			}
+		}
+		return vida.NilValue, nil
+	}
+}
--- stdlib/stdlib.go
+++ stdlib/stdlib.go
@@ -5,5 +5,6 @@ import "github.com/ever-eduardo/vida"
 func LoadStdlib() map[string]func() vida.Value {
 	l := make(map[string]func() vida.Value)
 	l["rand"] = generateRandom
+	l["math"] = generateMath
 	return l
 }
