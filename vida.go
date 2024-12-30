a461ca718ed84e38a52cab7f6d1ab2bdbabf4ff2
ever-eduardo <106024807+ever-eduardo@users.noreply.github.com>
2024-12-29 20:24:51 -0500

    Add about info

--- vida.go
+++ vida.go
@@ -12,7 +12,7 @@ const minor uint64 = 3
 const patch uint64 = 5
 const inception uint64 = 24
 const header uint64 = v<<56 | i<<48 | d<<40 | a<<32 | major<<24 | minor<<16 | patch<<8 | inception
-const name = "Vida 🌱🐝🌻 Programming Language"
+const name = "Vida 🌱🐝🌻"
 
 func Name() string {
 	return name
@@ -20,4 +20,24 @@ func Name() string {
 
 func Version() string {
 	return fmt.Sprintf("Version %v.%v.%v", major, minor, patch)
+}
+
+func About() string {
+	return "\n\n\t" + name + "\n\t" + Version() + `
+	
+	
+	What is Vida?
+
+	Vida is a simple, interpreted computer language.
+	The current implementation is written in Go.
+	It has a minimal set of language constructs making it ergonomic
+	and suitable for common tasks.
+	The language can be extended by implementing the Value interface.
+	The stdlib is implemented in such way, and you can add to the
+	stdlib all that you want for your projects.
+	Contributions are welcome.
+	Happy Vida coding!
+
+
+	`
 }
